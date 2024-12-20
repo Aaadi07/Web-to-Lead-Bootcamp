42fc7ffbf015c4fe126c7b7a9d9655c25b1dfd41
Aaadi07 <adityasharma4572@gmail.com>
2024-12-20 18:25:30 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function beforesubmit(){
-    let outputdate =document.querySelector(".outputdate");
-    let inputdate =document.querySelector(".inputdate");
-    console.log("inputdate.value", inputdate.value);   //string --> date(en_IN)
+let capthachacked = false;
+function beforesubmit(event){
+    if (capthachacked){
+        let outputdate =document.querySelector(".outputdate");
+        let inputdate =document.querySelector(".inputdate");
+        console.log("inputdate.value", inputdate.value);   //string --> date(en_IN)
+    
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formattedDate;
+    }else {
+        alert ("please check the recaptcha box to submit the lead");
+        event.preventDefault();
+    }
+   
+}
 
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formattedDate;
+    function timestamp() {
+         var response = document.getElementById("g-recaptcha-response"); 
+         if (response == null || response.value.trim() == "") {
+            var elems = JSON.parse(
+                document.getElementsByName("captcha_settings")[0].value
+            );
+
+            elems["ts"] = JSON.stringify(new Date().getTime());
+            document.getElementsByName("captcha_settings")[0].value = 
+            JSON.stringify(elems); 
+            
+            } 
+        } 
+setInterval(timestamp, 500); 
+
+function capthchasuccess(){
+    capthachacked = true;
 }
